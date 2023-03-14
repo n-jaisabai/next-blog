b00b67d242130efb639f07bfbe7b81fe2f8e25dd
NJ <nattapong.jaisabai@outlook.com>
2023-03-14 15:12:12 +0700

    feat(header): customized design

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -9,45 +9,43 @@ import ThemeSwitch from './ThemeSwitch'
 
 const LayoutWrapper = ({ children }) => {
   return (
-    <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
-          <div>
+    <>
+      <header className="supports-backdrop-blur:bg-white/60 sticky top-0 z-40 py-2 backdrop-blur sm:py-4">
+        <SectionContainer>
+          <div className="flex items-center justify-between">
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
+                <div className="inline-flex h-6 items-center text-center text-xl font-semibold transition-transform duration-500 hover:scale-110">
+                  <div className="rounded-lg bg-gradient-to-tr from-blue-400 to-blue-800 py-0.5 px-1 text-white">
+                    NJ
                   </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
+                  <div className="ml-1">VERSE</div>
+                </div>
               </div>
             </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  {link.title}
-                </Link>
-              ))}
+            <div className="flex items-center text-base leading-5">
+              <div className="hidden sm:block">
+                {headerNavLinks.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.href}
+                    className="p-1 font-medium text-gray-900 hover:text-gray-500 dark:text-gray-100 dark:hover:text-gray-400 sm:p-4"
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
+              <ThemeSwitch />
+              <MobileNav />
             </div>
-            <ThemeSwitch />
-            <MobileNav />
           </div>
-        </header>
+        </SectionContainer>
+      </header>
+      <SectionContainer>
         <main className="mb-auto">{children}</main>
         <Footer />
-      </div>
-    </SectionContainer>
+      </SectionContainer>
+    </>
   )
 }
 
